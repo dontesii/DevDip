b33d9b8360185fed3cd2f8cb3566da22ab69fd2c
dontesii <73188125+dontesii@users.noreply.github.com>
2021-05-13 17:36:31 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,6 @@
 provider "aws" {
   region = "us-west-1"
 }
-#-----------------------------------------
-
-
-variable "image_id" {
-  type = string
-  default = "ami-04e59c05167ea7bd5"
-}
-
-variable "flavor" {
-  type = string
-  default = "t2.micro"
-}
 
 
 #-----------------------------------------
