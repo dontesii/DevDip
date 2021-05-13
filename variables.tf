619a020643f312c7e9e30d2b1e366cef447c2cb1
dontesii <73188125+dontesii@users.noreply.github.com>
2021-05-13 14:01:03 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type = string
+  default = "us-west-1"
+}
+
+variable "image_id" {
+  type = string
+  default = "ami-04e59c05167ea7bd5"
+}
+
+variable "flavor" {
+  type = string
+  default = "t2.micro"
+}
+
+variable "ec2_instance_port" {
+  type = number
+  default = 80
+}
+
