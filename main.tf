046e9342d38b1d346eccaaddfc4bb47c76a79413
dontesii <73188125+dontesii@users.noreply.github.com>
2021-05-15 22:31:14 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,23 +11,25 @@ variable "app_subnets" {
 resource "aws_security_group" "alb-sec-group" {
   name = "alb-sec-group"
   description = "Security Group for the ELB (ALB)"
+dynamic "ingress" {
+    for_each = ["80", "443", "22", "8080"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
   egress {
-    from_port = 0
-    protocol = "-1"
-    to_port = 0
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    from_port = 80
-    protocol = "tcp"
-    to_port = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port = 443
-    protocol = "tcp"
-    to_port = 443
-    cidr_blocks = ["0.0.0.0/0"]
+
+  tags = {
+    Name  = "Dynamic SecurityGroup"
+    Owner = "Admon"
   }
 }
 #--------------------------------------------------
