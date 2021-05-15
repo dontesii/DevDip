5eaea5e784e00bb8479a3a756069af14d0ec7be7
dontesii <73188125+dontesii@users.noreply.github.com>
2021-05-15 20:44:46 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -114,11 +114,9 @@ resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.ELB.arn 
   port = 80
   protocol = "HTTP"
-
   default_action {
-  type = "forward"
-   
-  }
+    type = "forward"
+     }
 }
 #--------------------------------------------------
 resource "aws_vpc" "main" {
